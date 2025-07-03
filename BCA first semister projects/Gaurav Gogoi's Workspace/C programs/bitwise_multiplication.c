b0f88fc6738421f1f5eeb258f7ae364919ce9b7a
GCC-GRH <gauravgogoi079@gmail.com>
2025-07-04 00:29:14 +0530

    a

--- BCA first semister projects/Gaurav Gogoi's Workspace/C programs/bitwise_multiplication.c
+++ BCA first semister projects/Gaurav Gogoi's Workspace/C programs/bitwise_multiplication.c
@@ -7,3 +7,18 @@ void main(){
     num=num<<2;
     printf("\n %ld x 4 = %ld", tempnum, num);
 }
+
+/*num = 3;         // binary: 0000 0011
+num << 2 = 12;   // binary: 0000 1100
+
+
+
+
+What does << do?
+The expression num << 2 shifts all the bits of num to the left by 2 positions.
+
+Shifting left by 1 position (num << 1) multiplies the number by 2
+
+So, shifting left by 2 positions (num << 2) multiplies the number by 4
+
+*/
