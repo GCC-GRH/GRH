b7bd94770e2c01c4c6bcaeac5c04404bfdf18047
Hrishi123464 <barmahrishikesh9@gmail.com>
2025-08-14 21:56:36 +0530

    Update README.c

--- README.c
+++ README.c
@@ -1,12 +1,1 @@
-#include<stdio.h>
-int main (){
-  int a,b,c;
-  printf("enter the first no.:");
-  scanf("%d",&a);
-  printf("enter the second no.:");
-  scanf("%d",&b);
-  int c=a+b;
-  printf("The sum of the no. is %d",sum=a+b);
-  scanf("%d",&c);
-  return 0
-}
+
