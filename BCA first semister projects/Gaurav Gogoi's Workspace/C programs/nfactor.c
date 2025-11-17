2a8b5aba5e16057f659f64f335e58d8ee45cc661
GCC-GRH <217780031+GCC-GRH@users.noreply.github.com>
2025-11-17 09:14:31 +0530

    nfactor update

--- BCA first semister projects/Gaurav Gogoi's Workspace/C programs/nfactor.c
+++ BCA first semister projects/Gaurav Gogoi's Workspace/C programs/nfactor.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
+#include<stdlib.h>
 void main(){
+    system("cls");
     int n;
-    register int i;
+    register int i; // Register keyword is used to allocate and reserve spaces from cache memory for frequent access of a variable. It is not compulsory. Adv: Faster access time.
     printf("Please enter the number to find it's factor\n==>");
     scanf("%d",&n);
     for(i=1;i<n;i++){
