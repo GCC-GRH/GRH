1a85f2351e3706a8372059113d3005c0d3c28ef8
GCC-GRH <gauravgogoi079@gmail.com>
2026-02-25 01:25:37 +0530

    nn

---
+++ BCA II semester/USERS/Gaurav Gogoi's Workspace/Vectors/for-each-loop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector <char> vec={'G','A','U','R','A','V'};
+    // For-each loop is a vector specific loop which is used for traversing data items within a vector data struture.
+    for(char i: vec){ //In for-each loop, the iterator I iterates through vector data items. 
+        cout<<i;
+        cout<<" " <<endl;
+    }
+}
---
+++ BCA II semester/USERS/Gaurav Gogoi's Workspace/Vectors/vector.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector> // STL : Standard template library within which the predefined code for vectors are written.
+using namespace std;
+int main(){
+    vector <int> vec; // Declaring a vector data structure
+    vec = {1,2,3}; // Initialising a vector data structure
+    cout<<vec[0] <<endl; //printing vector data element contents
+}
+
+// Vectors are like array with indexing properties
+// Vectors can also be declared and initialised like this
+// vector <int> vec(3,0); here vec(number_of_data_elements,data_value to be initialised);
+
+// vector <int> vec(3,0) structure in memory
+// +----------------------------------+
+// |          |             |         |
+// |    0     |      0      |    0    | Data values initialised with 0 
+// |          |             |         | Total 3 data space reserved at once
+// +----------------------------------+
+//   idx: 0      idx/: 1      idx: 2
+/* Some of the vector functions include: 
+    1. vector.size();
+    2. vector.push_back;
+    3. vector.pop_back();
+    4. vector.front();
+    5. vector.back();
+    6. vector.at();
+*/
