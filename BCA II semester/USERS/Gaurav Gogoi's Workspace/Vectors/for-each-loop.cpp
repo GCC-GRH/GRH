f1ef7d6d14ce0e821fe11f248c467923c44012cb
GCC-GRH <gauravgogoi079@gmail.com>
2026-02-28 12:38:14 +0530

    Update for-each-loop.cpp

--- BCA II semester/USERS/Gaurav Gogoi's Workspace/Vectors/for-each-loop.cpp
+++ BCA II semester/USERS/Gaurav Gogoi's Workspace/Vectors/for-each-loop.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main(){
     vector <char> vec={'G','A','U','R','A','V'};
-    // For-each loop is a vector specific loop which is used for traversing data items within a vector data struture.
+    // For-each loop is a vector and array specific loop which is used for traversing data items within a vector data struture.
     for(char i: vec){ //In for-each loop, the iterator I iterates through vector data items. 
         cout<<i;
         cout<<" " <<endl;
